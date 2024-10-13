10036a5298c2a30c32c9877c9fb2a157c4d954bb
Ernesto J Ballon <ernestoballono@gmail.com>
2024-10-12 21:14:21 -0300

    adding metadata to blog pages

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -1,33 +1,58 @@
 import React from 'react';
 import { ReactNode } from 'react';
-import styles from './wordpressStyles.module.css';
 import { WpPostsType } from '../helpers/types';
 import { findPostBySlug } from '../helpers/graphql';
 import { postParser } from '../helpers/blogsParser';
 import { postSanitizer } from '../helpers/postSanitizer';
+import { Metadata } from 'next';
+
 type LayoutProps = {
   children: ReactNode;
   params: {
     slug: string;
   };
 };
+
 async function getPost(slug: string) {
   const wpPost: WpPostsType = await findPostBySlug(slug);
   const postItem = postSanitizer(postParser(wpPost));
   return postItem;
 }
-export default async function Layout({
-  children,
+
+export async function generateMetadata({
   params,
-  ...props
-}: LayoutProps) {
+}: LayoutProps): Promise<Metadata> {
   const { slug } = params;
   const blog = await getPost(slug);
 
-  console.log({
-    blog,
-    props,
-  });
+  return {
+    title: blog.title,
+    description: blog.excerpt,
+    openGraph: {
+      title: blog.title,
+      description: blog.excerpt,
+      images: [{ url: blog.featuredImage || '' }],
+      url: blog.slug,
+      type: 'article',
+      publishedTime: blog.date,
+      modifiedTime: blog.date,
+      authors: [blog.author || ''],
+      section: blog.categories?.join(', '),
+      tags: blog.categories,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: blog.title,
+      description: blog.excerpt,
+      images: [blog.featuredImage || ''],
+    },
+    other: {
+      'twitter:label1': 'Reading time',
+      'twitter:data1': '5 min read',
+    },
+  };
+}
 
-  return <main className={styles.wordpressContent}>{children}</main>;
+export default async function Layout({ children }: LayoutProps) {
+  return <>{children}</>;
 }
